5d0db633df2ee0616c8dc2f207731ee19d482da7
Kevin Kamto <kamtokevin@gmail.com>
2023-02-20 07:08:17 -0500

    chore(settle dialog): wip bloc code

---
+++ lib/dialogs/settle/bloc/settle_dialog_event.dart
@@ -0,0 +1,11 @@
+part of 'settle_dialog_bloc.dart';
+
+abstract class SettleDialogEvent extends Equatable {
+    const SettleDialogEvent();
+
+    @overrideList<Object?> get props => [];
+}
+
+class EnableExpiry extends SettleDialogEvent {
+    const 
+}
