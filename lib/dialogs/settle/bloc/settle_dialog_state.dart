da03916b1d43d127310566aabdb7958fa0fd185a
Kevin Kamto <kamtokevin@gmail.com>
2023-02-22 02:25:33 -0500

    feat: skeleton settle dialog completed

--- lib/dialogs/settle/bloc/settle_dialog_state.dart
+++ lib/dialogs/settle/bloc/settle_dialog_state.dart
@@ -1,38 +1,35 @@
 part of 'settle_dialog_bloc.dart';
 
 class SettleDialogState extends Equatable {
-    const SettleDialogState({
-    this.aptPair = AptPair.empty,
-    this.longApt = const Apt.empty(),
-    this.shortApt = const Apt.empty(),
-    this.longBalance = 0,
-    this.shortBalance = 0,
-    this.shortRedeemInput = 0,
-    this.longRedeemInput = 0,
-    this.receiveAmount = 0,
+
+  const SettleDialogState({
     this.status = BlocStatus.initial,
-    this.errorMessage = '',
-    this.failure = Failure.none,
-    this.spendAmount = 0,
-    this.collateralPerPair = 0,
-    this.smallestBalance = 0,
-    });
+    this.longAPTAmount = 0,
+    this.shortAPTAmount = 0,
+  });
 
-  final AptPair aptPair;
-  final Apt longApt;
-  final Apt shortApt;
-  final double longBalance;
-  final double shortBalance;
-  final double shortRedeemInput;
-  final double longRedeemInput;
-  final double receiveAmount;
+  final double longAPTAmount;
+  final double shortAPTAmount;
   final BlocStatus status;
-  final String errorMessage;
-  final Failure failure;
-  final double spendAmount;
-  final int collateralPerPair;
-  final double smallestBalance;
+  
+  @override
+  List<Object> get props {
+    return [
+      longAPTAmount,
+      shortAPTAmount,
+      status,
+    ];
+  }
 
-    
-
+  SettleDialogState copyWith({
+    double? longAPTAmount,
+    double? shortAPTAmount,
+    BlocStatus? status,
+  }) {
+    return SettleDialogState(
+      longAPTAmount: longAPTAmount ?? this.longAPTAmount,
+      shortAPTAmount: shortAPTAmount ?? this.shortAPTAmount,
+      status: status ?? this.status,
+    );
+  }
 }
